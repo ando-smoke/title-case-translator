dd22f6cfa1468cc620c4cf2d4bee0ed907a9f224
Andy Uppendahl <a.uppendahl@gmail.com>
2015-02-10 14:40:12 -0800

    Confirm functionality in titleCase method to accept word in all caps and return it in title case.

--- spec/specs.js
+++ spec/specs.js
@@ -2,4 +2,8 @@ describe("titleCase", function() {
   it("capitalizes the first letter of a word", function() {
     expect(titleCase("cat")).to.equal("Cat");
   });
+
+  it("takes a single word in uppercase and returns it in titlecase", function() {
+    expect(titleCase("CAT")).to.equal("Cat");
+  });
 });

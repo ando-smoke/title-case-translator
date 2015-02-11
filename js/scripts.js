a051458cfa8b4a8bb641cfe0cdf18fffd00f5889
Andy Uppendahl <a.uppendahl@gmail.com>
2015-02-10 18:08:39 -0800

    Add functionality to titleCase js method allowing for strings of more than two words to be titlecased, and considering special strings such as 'About a Boy' and 'Ladies and Gentlemen of the Jury'.

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,16 @@
 var titleCase = function(string) {
   var words = string.split(" ");
-  words = words.map(function(word) {
+  var nonTitleCaseWordsRE =
+    /\b(?:a|about|an|and|at|but|by|in|near|of|on|or|the|under|upon|with)\b/i;
+  words = words.map(function(word, index) {
     word = word.toLowerCase();
-    word = word.replace(word[0], word[0].toUpperCase());
+
+    if ((index === 0) || (nonTitleCaseWordsRE.test(word) === false)) {
+      word = word.replace(word[0], word[0].toUpperCase());
+    }
+
     return word;
   });
-  
+
   return words.join(" ")
 };
-
-// var titleCaseString = function(string) {
-//   // exceptionWordsRE = /and|but|by|on|the/;
-//   var words = string.split(" ");
-//
-//   return words.map(titleCase).join(" ");
-// };
--- spec/specs.js
+++ spec/specs.js
@@ -3,15 +3,32 @@ describe("titleCase", function() {
     expect(titleCase("cat")).to.equal("Cat");
   });
 
-  it("takes a single word in uppercase and returns it in titlecase", function() {
+  it("takes a single word in uppercase and returns it in \
+  titlecase", function() {
     expect(titleCase("CAT")).to.equal("Cat");
   });
 
-  it("takes a single word in a mixedcase format and returns it in titlecase", function() {
+  it("takes a single word in a mixedcase format and returns \
+  it in titlecase", function() {
     expect(titleCase("cAT")).to.equal("Cat");
   });
 
-  it("takes a string of two words all lowercased, and returns it titlecased", function() {
+  it("takes a string of two words all lowercased, and returns \
+  it titlecased", function() {
     expect(titleCase("cat power")).to.equal("Cat Power");
+  });
+
+  it("takes in the string 'snow white AND the seven dwarfs' \
+  and returns 'Snow White and the Seven Dwarfs'", function() {
+    expect(titleCase("snow white AND the seven dwarfs")).to.equal("Snow White and the Seven Dwarfs");
+  });
+
+  it("takes in the string 'Ladies And Gentlemen Of The Jury' \
+  and returns 'Ladies and Gentlemen of the Jury'", function() {
+    expect(titleCase("Ladies And Gentlemen Of The Jury")).to.equal("Ladies and Gentlemen of the Jury");
+  });
+
+  it("takes in the string 'about a boy' and returns 'About a Boy'", function() {
+    expect(titleCase("about a boy")).to.equal("About a Boy");
   });
 });
